69c5a070f32866b8ff39e20db7e09ad0f77d60b4
richarddela <richarddelaa12@gmail.com>
2016-06-19 11:13:29 -0500

    corrección de error

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-consol.log("Inicio de programa");
-var nombre = promt("Cual es tu nombre?")
-document.write("<h1> Su nombre es " + nombre + ".</p>")
+
+console.log("Inicio de programa");
+var nombre = prompt("Cual es tu nombre?");
+document.write("<h1> Su nombre es  + nombre + </h1>");
 console.log("Fin de programa");
